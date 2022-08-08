fd3c5f55358e9a4a0278891a95c91c87fa3f7cd5
niitwh <46929322+niitwh@users.noreply.github.com>
2022-08-08 17:29:01 +0800

    Update private_registry_auth_test.go

--- test/e2e/secretgencontroller/private_registry_auth_test.go
+++ test/e2e/secretgencontroller/private_registry_auth_test.go
@@ -97,7 +97,7 @@ spec:
 }
 
 func Test_PackageInstallAndRepo_CanAuthenticateToPrivateRepository_UsingPlaceholderSecret(t *testing.T) {
-	if os.Getenv("SKIP_NAMESPACED") == "true" {
+	if os.Getenv("KAPPCTRL_E2E_SKIP_IN_NS_SUPPORT") == "true" {
 		t.Skip("Skip in namespaced controller test")
 	}
 	env := e2e.BuildEnv(t)
